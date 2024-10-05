47fdcc3ea5bee7dd6c962e71ebcefb6c7e004b36
ismailmahmood11 <97909583+ismailmahmood11@users.noreply.github.com>
2024-10-05 13:42:10 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -32,7 +32,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "1b4d923a31d33cb7bd63794295acaee5
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "f4f22ef9cae94c7169a4495a3d06a125",
+"flutter_bootstrap.js": "8249ca89a330390818b2eb1b084485ac",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
